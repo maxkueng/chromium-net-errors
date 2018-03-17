9fc2a03be09de469642d1c6e4cd41374d15dc303
Max Kueng <me@maxkueng.com>
2018-03-17 18:45:15 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'semi-style': 'off',
+    'import/prefer-default-export': 'off',
+  },
+  env: {
+    node: true,
+  },
+};
